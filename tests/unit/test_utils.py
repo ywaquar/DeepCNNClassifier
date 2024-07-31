07738006460e1a560ebbbeed67c84068bd0c24ac
ywaquar <ywaquar@gmail.com>
2024-07-31 06:55:29 +0530

    test added

--- tests/unit/test_utils.py
+++ tests/unit/test_utils.py
@@ -4,23 +4,20 @@ from pathlib import Path
 from box import ConfigBox
 from ensure.main import EnsureError
 
+
 class Test_read_yaml:
-        
-    yaml_files = [
-        "tests/data/empty.yaml",
-        "tests/data/demo.yaml"
-    ]
 
-    def test_read_yaml_empty():
+    yaml_files = ["tests/data/empty.yaml", "tests/data/demo.yaml"]
+
+    def test_read_yaml_empty(self):
         with pytest.raises(ValueError):
-            read_yaml(Path(yaml_files[0]))
+            read_yaml(Path(self.yaml_files[0]))
 
-    def test_readyaml_return_type():
-        response = read_yaml(Path(yaml_files[-1]))
+    def test_read_yaml_return_type(self):
+        response = read_yaml(Path(self.yaml_files[-1]))
         assert isinstance(response, ConfigBox)
 
     @pytest.mark.parametrize("path_to_yaml", yaml_files)
-    def test_read_yaml_bad_type(path_to_yaml):
+    def test_read_yaml_bad_type(self, path_to_yaml):
         with pytest.raises(EnsureError):
-            # read_yaml(Path(yaml_files[path_to_yaml]))
             read_yaml(path_to_yaml)
